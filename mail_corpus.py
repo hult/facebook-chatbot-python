45bbd9c64fb19d22a04b19c3bc61081e38e154a3
Magnus Hult <magnus@magnushult.se>
2016-04-14 12:49:04 +0200

    Switch back to always write corpus.txt

--- mail_corpus.py
+++ mail_corpus.py
@@ -12,11 +12,11 @@ def main():
     """Extract the texts of emails from a specified mailbox and
     from a specified set of senders and write corpus.txt.
 
-    Usage: python mail_corpus.py output mboxfile email1@example.com email2@example.com
+    Usage: python mail_corpus.py mboxfile email1@example.com email2@example.com
     """
-    mbox = mailbox.mbox(sys.argv[2])
-    addresses = set(sys.argv[3:])
-    f = open(sys.argv[1], "w")
+    mbox = mailbox.mbox(sys.argv[1])
+    addresses = set(sys.argv[2:])
+    f = open("corpus.txt", "w")
     for text in mail_parser.mail_texts(mbox, addresses):
         print >> f, text
     f.close()
